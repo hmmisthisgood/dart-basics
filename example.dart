34775d46b90c1e090640fea5920b1a778dc37f43
surpriso1997 <maneesh10chaulagain@gmail.com>
2022-01-24 21:00:38 +0545

    creatred variables and example files

---
+++ example.dart
@@ -0,0 +1,22 @@
+main() {
+  print("hello world");
+
+// identifers:
+
+  /// for
+  /// each
+  /// var
+  /// int
+  /// string
+  /// bool
+  /// continue
+  /// break
+  /// if
+  /// else
+  /// else if
+  /// switch
+  /// null
+  
+  
+
+}
---
+++ variables.dart
@@ -0,0 +1,52 @@
+main() {
+// data types
+
+  /// primary data:
+  /// int = -2, -3, 0,1, 2,4 5,
+  /// uint= 0,1,2,3,4
+  /// bool= true, false
+  /// string = "this is text or stiring"
+  /// double= 1.0, 2.0,3.0
+  /// num= both int, double
+
+  //  var
+  // dynamic
+
+  // final
+  // const
+
+//  convention for varible names
+//  camelCase
+
+// convention for filename
+//  snake_case
+
+// int values
+
+  var number = 10.0;
+
+  // num data type
+  num dynamicNumber = 10.0;
+
+  print(dynamicNumber);
+
+  dynamicNumber = 5;
+  dynamicNumber = 11;
+
+  print(dynamicNumber.toString());
+
+  String randomValue = "this is string";
+  randomValue.length;
+
+  /// Non primary data types :
+  /// List (Equivalent Array, ArraryList)
+  /// Map (Equivalent of HashMap, Dictonary, Obejct)
+  /// Set
+
+  const APP_NAME = "Photo Viewer";
+
+  // APP_NAME = "this";
+
+  final currentTime = DateTime.now();
+  // currentTime = DateTime.now()
+}
